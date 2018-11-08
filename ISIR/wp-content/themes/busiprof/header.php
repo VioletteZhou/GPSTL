7cd4ba189c274872849809ded7dbef67444f7d79
Ziyi Zhou <zzyviolette@gmail.com>
2018-11-08 12:16:00 +0100

    creer projet et equipe

--- ISIR/wp-content/themes/busiprof/header.php
+++ ISIR/wp-content/themes/busiprof/header.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-<head>	
+<head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-	
+
 	<?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 	<?php endif; ?>
 
-<?php wp_head(); 
+<?php wp_head();
 $current_options = wp_parse_args(  get_option( 'busiprof_theme_options', array() ), theme_setup_data() );
-?>	
+?>
 </head>
 <body <?php body_class(); ?>>
 
-<!-- Navbar -->	
+<!-- Navbar -->
 <nav class="navbar navbar-default">
 	<div class="container">
 		<!-- Brand and toggle get grouped for better mobile display -->
@@ -26,7 +26,7 @@ $current_options = wp_parse_args(  get_option( 'busiprof_theme_options', array()
 					bloginfo('name');
 				}else{
 				?>
-				<img alt="<?php bloginfo("name"); ?>" src="<?php echo ( esc_url($current_options['upload_image']) ? $current_options['upload_image'] : get_template_directory_uri() . '/images/logo.png' ); ?>" 
+				<img alt="<?php bloginfo("name"); ?>" src="<?php echo ( esc_url($current_options['upload_image']) ? $current_options['upload_image'] : get_template_directory_uri() . '/images/logo.png' ); ?>"
 				alt="<?php bloginfo("name"); ?>"
 				class="logo_imgae" style="width:<?php echo esc_html($current_options['width']).'px'; ?>; height:<?php echo esc_html($current_options['height']).'px'; ?>;">
 				<?php } ?>
@@ -41,16 +41,16 @@ $current_options = wp_parse_args(  get_option( 'busiprof_theme_options', array()
 
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-			<?php 
+			<?php
 				wp_nav_menu( array(
 				'theme_location' => 'primary',
 				'container'  => 'nav-collapse collapse navbar-inverse-collapse',
 				'menu_class' => 'nav navbar-nav navbar-right',
 				'fallback_cb' => 'busiprof_fallback_page_menu',
-				'walker' => new busiprof_nav_walker()) 
-				); 
-			?>			
+				'walker' => new busiprof_nav_walker())
+				);
+			?>
 		</div>
 	</div>
-</nav>	
+</nav>
 <!-- End of Navbar -->
---
+++ ISIR/wp-content/themes/custom/test.php
@@ -0,0 +1,5 @@
+<?php
+  echo "sbnng";
+?>
+
+<?php get_footer(); ?>
