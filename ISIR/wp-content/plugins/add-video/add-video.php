58aab337bf35e8aa4d5446f516d580f0321a171c
Ziyi Zhou <zzyviolette@gmail.com>
2018-12-11 16:26:45 +0100

    insert video

---
+++ ISIR/wp-content/plugins/add-video/add-video.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Add videos
+Description: An user can add a video to his page
+Author: GPSTL
+*/
+
+// Include add-text-functions.php, use require_once to stop the script if mfp-functions.php is not found
+require_once plugin_dir_path(__FILE__) . 'includes/addvideo-functions.php';
